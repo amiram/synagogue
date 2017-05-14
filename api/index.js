7ca472b63d93b0b8bea32681d15f624ea3950ed0
amiram <amiramk@gmail.com>
2017-05-14 22:33:08 +0300

    port

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser());
 
 routes(app);
 
-const port = process.env.$PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   logger.info(`api started. listening on port ${port}`);
