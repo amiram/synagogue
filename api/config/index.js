3c3c6dca3cda1d0f1449ad0751b2d85067ee65ec
amiram <amiramk@gmail.com>
2017-05-16 00:02:31 +0300

    init

--- api/config/index.js
+++ api/config/index.js
@@ -7,9 +7,14 @@ const env = process.env.NODE_ENV;
 
 const envConfigPath = path.join(__dirname, `${env || 'development'}.js`);
 
+logger.info(`Trying to load env config from ${envConfigPath}`);
+
 if (fs.existsSync(envConfigPath)) {
+  logger.info(`Using environment ${env} config`);
   const envConfig = require(envConfigPath);
   _.merge(all, envConfig);
+} else {
+  logger.info(`Using default environment config`);
 }
 
 module.exports = all;
